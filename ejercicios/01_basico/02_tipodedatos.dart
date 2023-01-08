b3ff2c172f456cc347f547ebea53bf860c9e808e
Alejandro González Reyes <alexglzrys@gmail.com>
2023-01-08 14:17:28 -0600

    tipado - cadenas de texto

--- ejercicios/01_basico/02_tipodedatos.dart
+++ ejercicios/01_basico/02_tipodedatos.dart
@@ -1,4 +1,6 @@
 void main() {
+  // * Números
+
   // Declaración de variables de tipo numérico
   int edad = 37;
   double peso = 132.45;
@@ -15,4 +17,30 @@ void main() {
   // Esta instrucción es válida, por que la variable doctorado puede ser nula
   print(doctorado);
   print(cantidad);
+
+  // * Cadena de Caracteres
+
+  // Declaración de variables de tipo cadena
+  String nombre = 'Alejandro';
+  String apellidos = "González Reyes";
+
+  // El caracter de \ permite escapar caracteres especiales dento de una cadena de texto
+  String marca = 'Editorial O\'Really';
+  String nombre_completo = '$nombre $apellidos';
+
+  // Las triples comillas permiten escribir cadenas de múltiples líneas
+  String tyc = '''Términos y condiciones
+  Este es un documento escrito en los primeros
+  días del año 2023, dónde se especifican las cláusulas
+  del contrato de arrendamiento
+  ''';
+
+  // Dart permite escribir cadenas en formato crudo, sin que sea necesario escapar caracteres especiales
+  // solo basta anteponer el prefijo r antes del contenido de la cadena
+  String raw = r'C:\\usuario\carpeta\flutter\$bin';
+
+  print(nombre_completo);
+  print(marca);
+  print(tyc);
+  print(raw);
 }
